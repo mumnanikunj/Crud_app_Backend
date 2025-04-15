9dc536ec35e873d6d545888d7cfaf0d5a44bb727
nikunj <mumnanikunj6@gmail.com>
2025-04-15 18:57:51 +0530

    Refactor multer middleware: create temp directory if it doesn't exist and streamline file upload configuration

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,13 +1,23 @@
 import multer from "multer";
+import fs from "fs";
+import path from "path";
+
+// Define temp folder path
+const tempDir = path.join('public', 'temp');
+
+// Check if it exists; if not, create it
+if (!fs.existsSync(tempDir)) {
+  fs.mkdirSync(tempDir, { recursive: true });
+}
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, './public/temp')
-    },
-    filename: function (req, file, cb) {
-      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-      cb(null, file.fieldname + '-' + uniqueSuffix)
-    }
-  })
-  
-export const upload = multer({ storage })
+  destination: function (req, file, cb) {
+    cb(null, tempDir);
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    cb(null, file.fieldname + '-' + uniqueSuffix);
+  }
+});
+
+export const upload = multer({ storage });
