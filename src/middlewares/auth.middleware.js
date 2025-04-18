ac3a44472696a76aa08a8cedff01cd7e8b54ed50
nikunj <mumnanikunj6@gmail.com>
2025-04-18 16:35:05 +0530

    Refactor authentication middleware: improve JWT verification and enhance error handling; protect project routes with authentication

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,31 +1,28 @@
-import { User } from "../models/user.model.js"
-import { asyncHendler } from "../utils/asyncHendler.js"
-import jwt from "jsonwebtoken"
+import { User } from "../models/user.model.js";
+import { asyncHendler } from "../utils/asyncHendler.js";
+import jwt from "jsonwebtoken";
 
+export const verifyJWT = asyncHendler(async (req, res, next) => {
+  try {
+    const authHeader = req.headers.authorization || req.headers["authorization"];
+    const token = req.cookies?.accessToken || (authHeader && authHeader.replace("Bearer ", ""));
 
-export const verifyJWT = asyncHendler (async (req, res, next) =>{
-
-    try {
-        const token =  req.cookies?.accessToken || req.headers("authorization")?.replace("Bearer ", "")
-    
-        if(!token){
-            return res.status(401).json({statusCode: 401, data: [], message: "Unauthorized request"})  
-        }
-    
-        const decodedToken =  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
-    
-        const user =  await User.findById(decodedToken?._id).select("-password -refreshToken")
-    
-        if(!user){
-    
-            return res.status(401).json({statusCode: 401, data: [], message: "Invalid user"})
-        }
-    
-        req.user = user;
-        next()
-    } catch (error) {
-            console.log('invalid token',error)
-            return res.status(404).json({statusCode: 404, data: [], message: "Invalid token"})
+    if (!token) {
+      return res.status(401).json({ statusCode: 401, data: [], message: "Unauthorized request" });
     }
 
-})
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
+    const user = await User.findById(decodedToken?._id).select("-password -refreshToken");
+
+    if (!user) {
+      return res.status(401).json({ statusCode: 401, data: [], message: "Invalid user" });
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    console.log("Invalid token", error);
+    return res.status(403).json({ statusCode: 403, data: [], message: "Invalid token" });
+  }
+});
--- src/routes/project.routes.js
+++ src/routes/project.routes.js
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import {createProject, getAllProjects, getProjectById , UpdateProject ,DeleteProject} from "../controllers/project.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
-router.route('/createProject').post(createProject);
+router.route('/createProject').post(verifyJWT,createProject);
 router.route('/getAllProjects').get(getAllProjects);
-router.route('/getProjectById').get(getProjectById);
-router.route('/updateProject').put(UpdateProject);
-router.route('/deleteProject').delete(DeleteProject);
+router.route('/getProjectById').get(verifyJWT,getProjectById);
+router.route('/updateProject').put(verifyJWT,UpdateProject);
+router.route('/deleteProject').delete(verifyJWT,DeleteProject);
 
 
 export default router;
